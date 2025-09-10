63d94db0796769bf893a8426530ec90e9fc3f2a6
Anna Ro <roanna.97line@gmail.com>
2025-09-10 16:10:08 +0300

    feat: change theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react';
 import { MovieCard } from './MovieCard';
 import { MOVIES } from './movies.data';
 import { useDebounce } from './hooks/useDebounce';
+import { useTheme } from './hooks/useTheme';
 
 function App() {
+  const {theme, toggleTheme} = useTheme();
   const [searchTerm, setSearchTerm] = useState('');
   const debouncedSearch = useDebounce(searchTerm, 500);  
   const movies = MOVIES.filter(movie => 
@@ -11,7 +13,8 @@ function App() {
   );
 
   return (
-    <div className='min-h-screen w-full bg-black text-white px-6 py-5'>
+    <div className='min-h-screen w-full bg-white dark:bg-black 
+    text-black dark:text-white px-6 py-5'>
       <header className='mb-10 flex items-center justify-between'>
         <img
           src='/netflix.png'
@@ -19,15 +22,26 @@ function App() {
           className='h-8 w-auto'
         />
 
-        <input 
-          type='search' 
-          value={searchTerm}
-          onChange={(e) => {
-            setSearchTerm(e.target.value)
-          }} 
-          placeholder='Search...🔍'
-          className='border border-white/30 px-2 py-1 rounded outline-0'
-        />
+        <div>
+          <input 
+            type='search' 
+            value={searchTerm}
+            onChange={(e) => {
+              setSearchTerm(e.target.value)
+            }} 
+            placeholder='Search...🔍'
+            className='border border-black/15 dark:border-white/30 px-2 py-1 rounded outline-0'
+          />
+
+          <button
+            onClick={toggleTheme}
+            className='text-sm px-3 py-1rounded border border-white/20
+            dark:border-white/10 hover:bg-white hover:text-black
+            dark:hover:bg-ehite/10 transition w-20'
+          >
+            {theme === 'dark' ? 'Light☀️' : 'Dark🌙'}
+          </button>
+          </div>
 
       </header>
       <main className='flex gap-6'>
