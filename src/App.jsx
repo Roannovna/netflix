8ad1572f5f6280e2be720a8fa85ff73bde45424f
Anna Ro <roanna.97line@gmail.com>
2025-09-10 14:04:46 +0300

    feat: add search input and debounce hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import { MovieCard } from './MovieCard';
 import { MOVIES } from './movies.data';
+import { useDebounce } from './hooks/useDebounce';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const debouncedSearch = useDebounce(searchTerm, 500);  
+  const movies = MOVIES.filter(movie => 
+    movie.name.toLowerCase().includes(debouncedSearch.toLowerCase())
+  );
+
   return (
     <div className='min-h-screen w-full bg-black text-white px-6 py-5'>
       <header className='mb-10 flex items-center justify-between'>
@@ -10,13 +18,26 @@ function App() {
           alt='Netflix'
           className='h-8 w-auto'
         />
+
+        <input 
+          type='search' 
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value)
+          }} 
+          placeholder='Search...🔍'
+          className='border border-white/30 px-2 py-1 rounded outline-0'
+        />
+
       </header>
       <main className='flex gap-6'>
-        {
-          MOVIES.map( movie => (
+        {movies.length ? (
+          movies.map(movie => (
             <MovieCard {...movie} key={movie.name} />
-          ) )
-        }
+          )) 
+        ) : (
+          <p>No movies found🥲</p>
+        )}
       </main>
     </div>
   )
